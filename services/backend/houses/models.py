fefcc8f864727039d9527dc163e326a76c52133a
Cu7ious <garkusha.sergiy@gmail.com>
2024-12-04 18:11:44 -0800

    Added more symbols to the image field. Added ignore file

--- services/backend/houses/models.py
+++ services/backend/houses/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class House(models.Model):
     name = models.CharField(max_length=150)
-    image = models.CharField(max_length=150)
+    image = models.CharField(max_length=350)
     description = models.TextField()
     checks = models.PositiveIntegerField(default=0)
     likes = models.PositiveIntegerField(default=0)
